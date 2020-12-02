23eb8416ba6ad75f8386701fe4d57506d34bb8de
Luis Sousa <lv-ss@hotmail.com>
2020-12-01 21:24:33 -0300

    finalizando resolução"

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const eplusDevList = ['Parça', 'Galego', 'Xará', 'Baqueta', 'Na', 'Lindo', 'Pablito', 'Bahia']
 
 function listDevs(list){
-
+  list.forEach(dev => {
+    console.log(dev);
+  });
 }
 
 listDevs(eplusDevList)
